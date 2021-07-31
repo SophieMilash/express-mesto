01653ef2fc79dd57be2e41d9d5253d8f8ee9476a
Sophie <sophie.milash@yandex.ru>
2021-07-31 13:48:39 +0200

    feat: add controllers and routers for users

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.listen(PORT);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '610538432abf444694287003',
+  };
+
+  next();
+});
+
+app.use('/users', require('./routes/user'));
+
+app.listen(PORT);
